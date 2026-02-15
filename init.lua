54d0bacabac325ff4848a56fb942794cb68ba374
Noah <nkugler2@asu.edu>
2026-02-14 23:30:32 -0700

    Inital download of lazy.nvim. NOT lazyvim, but rather the package manager lazy.nvim. Do not have any plugins installed, so that part of the code in lazy.lua on line 28 is commented out. I will uncomment when I download my first plugin

--- init.lua
+++ init.lua
@@ -1,8 +1,10 @@
--- Keep mapleader before any other keybind
--- WARNING: This doesn't work until I set up custom keymaps like <leader>w for save
-vim.g.mapleader = ' '
+-- lazy.nvim
+require("config.lazy")
 
+-- My Config
 require("noah.options")
 require("noah.keybinds")
 require("noah.autocmds")
 require("noah.languages")
+
+-- My Plugins
